7830e8814f368040fdfdf1c1b318b82f5eed8d67
jatin1213 <jatin.aggarwal.15csc@bml.edu.in>
2024-06-29 10:53:34 +0530

    commneted code remocved

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,7 @@
 import { combineReducers, createStore } from 'redux';
-import wishlistReducer, {
-    ADDWHISHLIST_PROFUCT,
-    REMOVEWHISHLIST_PROFUCT
-} from './wishlistReducer'
-import allProductList from './productsReducer';
-import cartReducer, {
-    ADD_PRODUCT,
-    REMOVE_PRODUCT,
-    ADD_PRODUCT_QUANTITY,
-    REDUCE_PRODUCT_QUANTITY
-} from './cartReducer';
+import wishlistReducer from './slice/wishlistReducer'
+import allProductList from './slice/productsReducer';
+import cartReducer from './slice/cartReducer';
 
 let reducer = combineReducers({
     productList: allProductList,
@@ -19,33 +11,3 @@ let reducer = combineReducers({
 
 
 export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__?.());
-
-
-// Here is the sample code and checking for reducer functionality
-
-// Add product to cartItem
-// store.dispatch(ADD_PRODUCT(1, 1));
-
-// Add product to cartItem
-// store.dispatch(ADD_PRODUCT(2, 1));
-
-// Add product to cartItem
-// store.dispatch(ADD_PRODUCT(3, 1));
-
-// remove product from cartItem
-// store.dispatch(REMOVE_PRODUCT(2, 0));
-
-// increase product quantity by
-// store.dispatch(ADD_PRODUCT_QUANTITY(3, 4));
-
-// decrease product quantity by
-// store.dispatch(REDUCE_PRODUCT_QUANTITY(3, 1));
-
-// add product to wishList
-// store.dispatch(ADDWHISHLIST_PROFUCT(1, 1));
-
-// add product to wishList
-// store.dispatch(ADDWHISHLIST_PROFUCT(2, 1));
-
-// remove product to wishList
-// store.dispatch(REMOVEWHISHLIST_PROFUCT(2, 1));
