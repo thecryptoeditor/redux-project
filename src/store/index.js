7a7dc0722d24ed53ebc3e4119299f19a7aa32381
jatin1213 <jatin.aggarwal.15csc@bml.edu.in>
2024-07-01 12:50:00 +0530

    createStore replaced with configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,12 @@
-import { combineReducers, createStore } from 'redux';
 import wishlistReducer from './slice/wishlistSlice'
 import allProductList from './slice/productsSlice';
 import cartReducer from './slice/cartSlice';
 import { configureStore } from '@reduxjs/toolkit'
 
-let reducer = combineReducers({
-    productList: allProductList,
-    cartItem: cartReducer,
-    wishList: wishlistReducer
-})
-
-export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__?.());
+export const store = configureStore({
+    reducer: {
+        productList: allProductList,
+        cartItem: cartReducer,
+        wishList: wishlistReducer
+    }
+});
