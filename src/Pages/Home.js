00935a7dd50a3758580aaea148e983134fe2b132
jatin1213 <jatin.aggarwal.15csc@bml.edu.in>
2024-07-01 22:03:22 +0530

    redux toolkit implemented for product slice

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Product from '../components/product.js';
+import { setProductList } from '../store/slice/productsSlice'
 
 export default function Home () {
+
+    let dispatch = useDispatch();
 
     let productList = useSelector((state) => {
         return state.productList;
     })
+
+    useEffect(() => {
+
+        // Fetching data from API and seting data to middleware
+        const getProductList = async () => { 
+            
+            try {
+                
+                let response = await fetch('https://fakestoreapi.com/products');
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                let data = await response.json();
+
+                // pushing data to middleware
+                dispatch(setProductList(data));
+
+            }
+            catch(err) {
+                console.log(err);
+            }
+        }
+
+        getProductList();
+
+    }, []);
 
     return (
         <>
--- src/store/slice/productsSlice.js
+++ src/store/slice/productsSlice.js
@@ -1,5 +1,15 @@
-import {productList} from '../productList.js'
+import { createSlice } from "@reduxjs/toolkit";
 
-export default function productsReducer (state = productList, action) {
-    return state;
-}
+const slice = createSlice({
+    name: 'products',
+    initialState: [],
+    reducers: {
+        setProductList(state, action) {
+            return action.payload
+        }
+    }
+})
+
+export const { setProductList } = slice.actions;
+
+export default slice.reducer;
